6860dde0fa905ba587d796aa78c4d2f7ff63b5a5
Hasini923 <62468629+Hasini923@users.noreply.github.com>
2025-05-04 14:13:09 +0530

    Add files via upload

---
+++ Circular_and_Doubly_LinkedList/header.h
@@ -0,0 +1,121 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+class cll{
+    private:
+    struct node{
+        int data;
+        struct node * link;
+    }*head;
+    public:
+        cll(){
+            head=NULL;
+        }
+        void insertbeg(int);
+        void insertend(int);
+        void insertprocess(int);
+        void execute(int);
+        int deletebeg();
+        void display();
+};
+
+
+void cll::insertbeg(int num){
+    struct node * newnode=(struct node *) malloc(sizeof(struct node));
+    newnode->data=num;
+    if (head==NULL){
+        newnode->data=num;
+        newnode->link=newnode;
+        head=newnode;
+    }
+    else{
+        struct node*temp=head;
+        newnode->link=head;
+        while (temp->link != head){
+            temp=temp->link;
+        }
+        temp->link=newnode;
+        head=newnode;
+    }
+    printf("\nThe number is inserted\n\n");
+}
+
+void cll::insertend(int num){
+    struct node * newnode=(struct node*) malloc(sizeof(struct node));
+    struct node * temp=head;
+    newnode->data=num;
+    if (head==NULL){
+        insertbeg(num);
+    }
+    else{
+        while(temp->link != head){
+            temp=temp->link;
+        }
+        temp->link=newnode;
+        temp->link->link=head;
+        printf("\nThe number is inserted\n\n");
+    }
+}
+
+
+void cll::insertprocess(int num){
+    insertend(num);
+}
+
+
+int cll::deletebeg(){
+    struct node * temp1=head;
+    int count=1;
+    while(temp1->link != head){
+        count++;
+        temp1=temp1->link;
+    }
+    if (count==1){
+        int val1=head->data;
+        head=NULL;
+        return val1;
+    }
+    else{
+        struct node * temp2=head;
+        int val=temp2->data;
+        while (temp2->link != head){
+            temp2=temp2->link;
+        }
+        temp2->link=head->link;
+        struct node *temp3=head;
+        head=head->link;
+        temp3->link=NULL;
+        return val;
+    }
+}
+
+
+void cll::display(){
+    struct node * temp=head;
+    printf("\n");
+    if (head==NULL){
+        printf("\nThe list is empty\n\n");
+    }
+    else{
+        while (temp->link != head){
+            printf("%d -> ", temp->data);
+            temp=temp->link;
+        }
+        printf("%d\n\n", temp->data);
+    }
+   
+}
+
+
+void cll::execute(int shatime){
+    int value=deletebeg();
+    int number=value-shatime;
+    if (number>0){
+        insertend(number);
+    }
+    else{
+        printf("\nThe number is deleted from the list\n\n");
+    }
+
+}
